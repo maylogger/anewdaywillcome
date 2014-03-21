5bb6d231c99b38c7b99c067b593e36135f673ced
evenwu <evendesign@gmail.com>
2014-03-22 00:18:52 +0800

    relate css img

--- config.rb
+++ config.rb
@@ -14,7 +14,7 @@ javascripts_dir = "javascripts"
 output_style = :compressed
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
-# relative_assets = true
+relative_assets = true
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
 # line_comments = false
@@ -26,18 +26,3 @@ line_comments = false
 # and then run:
 # sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
 preferred_syntax = :scss
-
-css_dir = "stylesheets" # by Fire.app 
-sass_dir = "sass" # by Fire.app 
-images_dir = "images" # by Fire.app 
-javascripts_dir = "javascripts" # by Fire.app 
-fireapp_coffeescripts_dir = "coffeescripts" # by Fire.app 
-fireapp_livescripts_dir = "livescripts" # by Fire.app 
-fireapp_minifyjs_on_build = false # by Fire.app 
-fireapp_always_report_on_build = true # by Fire.app 
-output_style = :nested # by Fire.app 
-relative_assets = false # by Fire.app 
-line_comments = false # by Fire.app 
-sass_options = {:debug_info=>false} # by Fire.app 
-fireapp_coffeescript_options = {:bare=>false} # by Fire.app 
-fireapp_livescript_options = {:bare=>false} # by Fire.app 
